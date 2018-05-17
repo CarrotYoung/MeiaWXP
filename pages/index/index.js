8d10a7d514944d4467723bc62225de83842ecba0
YangGuohao <765463821@qq.com>
2018-05-17 11:54:43 +0800

    Initial commit

---
+++ pages/index/index.js
@@ -0,0 +1,14 @@
+var printImg = '../../icons/print_green.png'
+var signInImg = '../../icons/sign_in.png'
+var printTxt = '打印参会证'
+var signInTxt = '入场签到'
+
+Page({
+  data: {
+    ixdcLogoIc: getApp().globalData.ixdcLogoIc,
+    printImg: printImg,
+    signInImg: signInImg,
+    printTxt: printTxt,
+    signInTxt: signInTxt
+  }
+})
